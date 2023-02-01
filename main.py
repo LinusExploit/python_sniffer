8b39ffc8f3f24c856df6694940411386f3af5ca8
Elmo <moh.hsj@gmail.com>
2023-02-01 21:26:25 +0400

    first commit only a one packet raw sniffer

---
+++ main.py
@@ -0,0 +1,33 @@
+import socket
+import os
+
+HOST = '192.168.1.216'
+
+
+def main():
+    # create a raw socket and bind to the interface
+    if os.name == "nt":
+        socket_protocol = socket.IPPROTO_IP
+    else:
+        socket_protocol = socket.IPPROTO_ICMP
+
+    sniffer = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket_protocol)
+    sniffer.bind((HOST, 0))
+
+    #Include the IP header in the capture
+    sniffer.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
+
+    if os.name == 'nt':
+        sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
+
+    # Read one packet
+    print(sniffer.recvfrom(65565))
+
+    # if we are on windows turn off promiscouos mode
+
+
+    if os.name == 'nt':
+        sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_OFF)
+
+if __name__ == '__main__':
+    main()
